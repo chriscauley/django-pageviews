fbfba526222666165c7389dae62982ee9925ca6e
chriscauley <chris@lablackey.com>
2014-04-06 14:53:47 +0000

    apparently there are some strings that cannot be urllib2.quote-ed

--- pageviews/middleware.py
+++ pageviews/middleware.py
@@ -5,7 +5,10 @@ import urllib2
 
 class PageViewsMiddleware:
     def process_request(self, request, *args, **kwargs):
-        url = urllib2.quote(request.path)
+        try:
+            url = urllib2.quote(request.path)
+        except KeyError:
+            url = "BAD UNICODE DATA"
         hit, hit_created = HitCount.objects.get_or_create(url=request.path)
         hit.hits = F('hits') + 1
         hit.save()
