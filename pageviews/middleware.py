68ed37bd0a786f2bed4ebf2a7dc0fcf97ac1999c
chriscauley <chris@lablackey.com>
2014-04-06 14:47:48 +0000

    Fixing OperationalError caused by weird unicode characters and MySQL

--- pageviews/middleware.py
+++ pageviews/middleware.py
@@ -1,9 +1,11 @@
 from .models import HitCount
 from django.db.models import F
 
+import urllib2
 
 class PageViewsMiddleware:
     def process_request(self, request, *args, **kwargs):
+        url = urllib2.quote(request.path)
         hit, hit_created = HitCount.objects.get_or_create(url=request.path)
         hit.hits = F('hits') + 1
         hit.save()
